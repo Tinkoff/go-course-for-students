8cff051a186f90d300c7345a5cc87cfe348e517d
Girag <37718971+Girag@users.noreply.github.com>
2023-02-21 21:19:04 +0200

    My implementation of homework 1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,19 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var result string
+
+	if i%15 == 0 {
+		result = "FizzBuzz"
+	} else if i%5 == 0 {
+		result = "Buzz"
+	} else if i%3 == 0 {
+		result = "Fizz"
+	} else {
+		result = strconv.Itoa(i)
+	}
+
+	return result
 }
