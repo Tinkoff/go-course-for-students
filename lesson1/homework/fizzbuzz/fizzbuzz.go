0ebf3cb1135675aea5c2f13d61f6dcc40d4522a0
Dmitrii Rogulin <dima.rogulin@gmail.com>
2023-02-21 19:30:33 +0300

    FizzBuzz implement Boilerplate in tests refactored

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,22 @@
 package fizzbuzz
 
+import (
+	"strconv"
+)
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var ans string
+
+	if i%3 == 0 {
+		ans += "Fizz"
+	}
+	if i%5 == 0 {
+		ans += "Buzz"
+	}
+
+	if len(ans) == 0 {
+		ans = strconv.Itoa(i)
+	}
+
+	return ans
 }
