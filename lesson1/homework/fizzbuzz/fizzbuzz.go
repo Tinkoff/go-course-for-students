230a308ab880ace6ed41c9fe6d757137294e8ed5
mirgalieva <mirgalieva.ai@phystech.edu>
2023-02-21 22:59:05 +0300

    homework1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var str string
+	if i%15 == 0 {
+		str = "FizzBuzz"
+	} else if i%3 == 0 {
+		str = "Fizz"
+	} else if i%5 == 0 {
+		str = "Buzz"
+	} else {
+		str = strconv.Itoa(i)
+	}
+	return str
 }
