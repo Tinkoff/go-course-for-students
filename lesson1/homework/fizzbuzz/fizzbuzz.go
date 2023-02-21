50506bc8259d55e0bb5dc6ec936b77b5047b871d
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2023-02-21 19:47:28 +0300

    [task-01] add fizzbuzz code

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,28 @@
 package fizzbuzz
 
+import (
+	"strings"
+	"strconv"
+	)
+	
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	
+	
+	
+	var res strings.Builder
+	
+	
+	
+	const FIZZ_MULT = 3
+	const BUZZ_MULT = 5
+	if i % FIZZ_MULT == 0 {
+	res.WriteString("Fizz")
+	}
+	if i % BUZZ_MULT == 0 {
+	res.WriteString("Buzz")
+	}
+	if (len(res.String()) == 0) {
+	res.WriteString(strconv.Itoa(i))
+	}
+	return res.String()
 }
