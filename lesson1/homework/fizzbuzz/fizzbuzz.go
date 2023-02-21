c470d00a88899d18f28f7d4da75fdcb113e3b7b3
DimaUtyuz <dima-utyuzhnikov2013@yandex.ru>
2023-02-21 20:46:09 +0300

    added solution

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,22 @@
 package fizzbuzz
 
+import "strconv"
+
+const (
+	Fizz = "Fizz"
+	Buzz = "Buzz"
+)
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var result string
+	if i%3 == 0 {
+		result += Fizz
+	}
+	if i%5 == 0 {
+		result += Buzz
+	}
+	if len(result) == 0 {
+		result = strconv.Itoa(i)
+	}
+	return result
 }
