7a954cd4f4059bc2c085f7573e359eaba86880a7
Daria Kovzhun <dariakovzhun@gmail.com>
2023-02-21 20:05:24 +0300

    done

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,19 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var res string
+
+	if i%15 == 0 {
+		res = "FizzBuzz"
+	} else if i%3 == 0 {
+		res = "Fizz"
+	} else if i%5 == 0 {
+		res = "Buzz"
+	} else {
+		res = strconv.Itoa(i)
+	}
+
+	return res
 }
