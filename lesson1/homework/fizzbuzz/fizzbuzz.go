964016756d1990e0ecdc44ee138accfe1b31477d
priamoryki <60192758+priamoryki@users.noreply.github.com>
2023-02-21 20:30:17 +0300

    add HW-1 solution

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,18 @@
 package fizzbuzz
 
-func FizzBuzz(i int) string {
-	// TODO
+import "strconv"
+
+func helpF(d int, s string, i int) string {
+	if i%d == 0 {
+		return s
+	}
 	return ""
+}
+
+func FizzBuzz(i int) string {
+	result := helpF(3, "Fizz", i) + helpF(5, "Buzz", i)
+	if result == "" {
+		return strconv.Itoa(i)
+	}
+	return result
 }
