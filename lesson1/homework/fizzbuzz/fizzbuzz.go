2a5b72abd6f81d9c9d80ca75677c2f84d7fa97b5
snp <niksel2002@gmail.com>
2023-02-21 22:39:06 +0300

    hw1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,18 @@
 package fizzbuzz
 
+import (
+	"strconv"
+)
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	switch 0 {
+	case i % 15:
+		return "FizzBuzz"
+	case i % 3:
+		return "Fizz"
+	case i % 5:
+		return "Buzz"
+	default:
+		return strconv.Itoa(i)
+	}
 }
