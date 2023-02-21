4b4537f505362ced9b9a9195bb43a11947d2307b
Alexander Romanov <xellarcode@gmail.com>
2023-02-21 20:23:18 +0300

    [hw-1] homework attempt 1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,19 @@
 package fizzbuzz
 
-func FizzBuzz(i int) string {
-	// TODO
-	return ""
+import "fmt"
+
+func FizzBuzz(i int) (res string) {
+	var check bool
+	if i%3 == 0 {
+		res += "Fizz"
+		check = true
+	}
+	if i%5 == 0 {
+		res += "Buzz"
+		check = true
+	}
+	if !check {
+		res = fmt.Sprintf("%d", i)
+	}
+	return
 }
