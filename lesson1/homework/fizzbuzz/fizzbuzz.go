1d8c2b49fd769150c73c5d576e445eaaa6c68835
Artyom Viryutin <artem.viryutin@tevian.ru>
2023-02-22 16:57:49 +0300

    implement fizzbuzz.FizzBuzz

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,18 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	multipleOfThree := i%3 == 0
+	multipleOfFive := i%5 == 0
+
+	if multipleOfThree && multipleOfFive {
+		return "FizzBuzz"
+	} else if multipleOfThree {
+		return "Fizz"
+	} else if multipleOfFive {
+		return "Buzz"
+	}
+
+	return strconv.Itoa(i)
 }
