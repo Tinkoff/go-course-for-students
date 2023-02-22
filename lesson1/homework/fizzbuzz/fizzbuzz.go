d2dc3a3ccd6332616a4889d6496250034ba962c7
Egor Perevoshchikov <egor_perevoshchikov@mail.ru>
2023-02-23 00:03:41 +0300

    Реализовал ф-ию FizzBuzz Написал цикл в main.go, в котором вызывается FizzBuzz Написал еще один тестик для FizzBuzz, в котором FizzBuzz принимает 0 и возвращает "FizzBuzz" (т.к. 0 % 15 == 0)

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,16 @@
 package fizzbuzz
 
-func FizzBuzz(i int) string {
-	// TODO
-	return ""
+import "strconv"
+
+func FizzBuzz(i int) (response string) {
+	if i%15 == 0 {
+		response = "FizzBuzz"
+	} else if i%5 == 0 {
+		response = "Buzz"
+	} else if i%3 == 0 {
+		response = "Fizz"
+	} else {
+		response = strconv.Itoa(i)
+	}
+	return
 }
