4332e600d6dedfb48ef4c20dd2025ea8cdf2a0fd
Alexey Kondratev <samarec1812@mail.ru>
2023-02-22 11:59:06 +0300

    [hw1] new solution

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,14 @@
 package fizzbuzz
 
+import "fmt"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	if i%15 == 0 {
+		return "FizzBuzz"
+	} else if i%3 == 0 {
+		return "Fizz"
+	} else if i%5 == 0 {
+		return "Buzz"
+	}
+	return fmt.Sprintf("%d", i)
 }
