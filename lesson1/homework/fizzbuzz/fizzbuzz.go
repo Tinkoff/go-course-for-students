23d249aeb4ba2dff718bd3525389c317945b3089
Tycoon <ntpmosakk070601@gmail.com>
2023-02-22 14:41:11 +0300

    added solution for hw1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,20 @@
 package fizzbuzz
 
+import (
+	"strconv"
+	"strings"
+)
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var sb strings.Builder
+	if i%3 == 0 {
+		sb.WriteString("Fizz")
+	}
+	if i%5 == 0 {
+		sb.WriteString("Buzz")
+	}
+	if sb.Len() == 0 {
+		sb.WriteString(strconv.Itoa(i))
+	}
+	return sb.String()
 }
