b28e2dca8d92415be6f43d6e8c8575b82b2e37c2
Alexander Romanov <xellarcode@gmail.com>
2023-02-22 22:27:12 +0300

    remove check variable

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -3,16 +3,13 @@ package fizzbuzz
 import "fmt"
 
 func FizzBuzz(i int) (res string) {
-	var check bool
 	if i%3 == 0 {
 		res += "Fizz"
-		check = true
 	}
 	if i%5 == 0 {
 		res += "Buzz"
-		check = true
 	}
-	if !check {
+	if len(res) == 0 {
 		res = fmt.Sprintf("%d", i)
 	}
 	return
