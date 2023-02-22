a3390b1087a01221fa2366926950066733cdfdb8
Rast-Dan <rast.dan2017@gmail.com>
2023-02-22 18:22:00 +0300

    add fizz buzz function and main code

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,20 @@
 package fizzbuzz
 
+import "strconv"
+
+const FIZZ = "Fizz"
+
+const BUZZ = "Buzz"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	if i%15 == 0 {
+		return FIZZ + BUZZ
+	}
+	if i%5 == 0 {
+		return BUZZ
+	}
+	if i%3 == 0 {
+		return FIZZ
+	}
+	return strconv.Itoa(i)
 }
