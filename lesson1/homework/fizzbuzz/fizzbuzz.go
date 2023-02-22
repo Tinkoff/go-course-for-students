ab1db0316abccffb9a23e640b3f2bb5d703d9d4e
Igor <zis02072000@gmail.com>
2023-02-22 16:58:46 +0300

    first commit

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,24 @@
 package fizzbuzz
 
+import "strconv"
+
+/*
+Напишите программу, которая выводит на экран числа от 1 до 100.
+
+При этом вместо чисел, кратных трем, программа должна выводить слово Fizz, а вместо чисел, кратных пяти — слово Buzz.
+
+Если число кратно пятнадцати, то программа должна выводить слово FizzBuzz.
+*/
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	switch {
+	case i%3 == 0 && i%15 != 0:
+		return "Fizz"
+	case i%5 == 0 && i%15 != 0:
+		return "Buzz"
+	case i%15 == 0:
+		return "FizzBuzz"
+	default:
+		return strconv.Itoa(i)
+	}
 }
