f81fcc15be0ebd39d7750434dadb2fad756ca3c9
a13xbb <alex.blokh24@gmail.com>
2023-02-22 15:52:47 +0300

    homework 1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var val string
+	if i%3 == 0 {
+		val += "Fizz"
+	}
+	if i%5 == 0 {
+		val += "Buzz"
+	}
+	if val == "" {
+		return strconv.Itoa(i)
+	}
+	return val
 }
