c8211596c5bb212399c2bc08a6800bc7d17f1abd
Roman Gostilo <romanngostilo@gmail.com>
2023-02-22 18:03:09 +0300

    add completed hw #1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,16 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	switch remainder := i % 15; {
+	case remainder == 0:
+		return "FizzBuzz"
+	case remainder%5 == 0:
+		return "Buzz"
+	case remainder%3 == 0:
+		return "Fizz"
+	default:
+		return strconv.Itoa(i)
+	}
 }
