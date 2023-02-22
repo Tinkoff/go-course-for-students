f7b625ffcb5155bee6a16a354424498045e9257b
Artyom Viryutin <artem.viryutin@tevian.ru>
2023-02-22 16:57:49 +0300

    implement fizzbuzz.FizzBuzz

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,21 @@
 package fizzbuzz
 
+import "strconv"
+
+// FizzBuzz returns Fizz if i is divided by 3, Buzz if i is divided by 5,
+// FizzBuzz if i is divided by 15, and just i otherwise
+//
+// Note the number is divided by 15 iff it is divided by 3 and 5
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var text string
+	if i%3 == 0 {
+		text += "Fizz"
+	}
+	if i%5 == 0 {
+		text += "Buzz"
+	}
+	if len(text) == 0 {
+		text = strconv.Itoa(i)
+	}
+	return text
 }
