4945505db30e1e57d9c3ff8b6f94e4c7be9ec648
Katherine <liponina.katya@yandex.ru>
2023-02-23 12:27:02 +0300

    implemented fizzbuzz

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,19 @@
 package fizzbuzz
 
+import (
+	"strconv"
+  )
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var result string = ""
+	if i % 3 == 0 {
+		result += "Fizz"
+	}
+	if i % 5 == 0 {
+		result += "Buzz"
+	}
+	if (result == "") {
+		return strconv.Itoa(i)
+	}
+	return result
 }
