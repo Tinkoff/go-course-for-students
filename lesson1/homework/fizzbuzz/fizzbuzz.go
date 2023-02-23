a6b654ab63312b0a944e8226268558001c2b21c3
mirgalieva <mirgalieva.ai@phystech.edu>
2023-02-23 17:21:24 +0300

    remove else construction

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -3,15 +3,15 @@ package fizzbuzz
 import "strconv"
 
 func FizzBuzz(i int) string {
-	var str string
+	var str = strconv.Itoa(i)
+	if i%3 == 0 {
+		str = "Fizz"
+	}
+	if i%5 == 0 {
+		str = "Buzz"
+	}
 	if i%15 == 0 {
 		str = "FizzBuzz"
-	} else if i%3 == 0 {
-		str = "Fizz"
-	} else if i%5 == 0 {
-		str = "Buzz"
-	} else {
-		str = strconv.Itoa(i)
 	}
 	return str
 }
