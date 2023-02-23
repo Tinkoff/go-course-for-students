783b587535aa0d6dfd6116d331eda7a535a55dd8
Сафронов Артем Андреевич <AAndSafronov@sberbank.ru>
2023-02-23 19:34:29 +0300

    do go fmt

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -3,11 +3,11 @@ package fizzbuzz
 import "strconv"
 
 func FizzBuzz(i int) string {
-	if i % 15 == 0 {
+	if i%15 == 0 {
 		return "FizzBuzz"
-	} else if i % 5 == 0 {
+	} else if i%5 == 0 {
 		return "Buzz"
-	} else if i % 3 == 0 {
+	} else if i%3 == 0 {
 		return "Fizz"
 	} else {
 		return strconv.Itoa(i)
