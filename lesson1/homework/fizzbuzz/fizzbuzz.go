ed6bf595b348972e6bd6f18a3dc6bf06893d0f3e
AndrewDanilin <andnilin@gmail.com>
2023-02-23 15:43:14 +0300

    fizzbuzz implemented

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
-func FizzBuzz(i int) string {
-	// TODO
-	return ""
+import "strconv"
+
+func FizzBuzz(i int) (result string) {
+	switch {
+	case i % 15 == 0: 
+		result = "FizzBuzz"
+	case i % 3 == 0:  
+		result = "Fizz"
+	case i % 5 == 0:  
+		result = "Buzz"
+	default:          
+		result = strconv.Itoa(i)
+	}
+	return 
 }
