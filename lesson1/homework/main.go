5a81d41ede9577031c6c23aac17e2fb973b5cdcf
Сафронов Артем Андреевич <AAndSafronov@sberbank.ru>
2023-02-23 19:15:52 +0300

    first commit FizzBuzz

--- lesson1/homework/main.go
+++ lesson1/homework/main.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	// TODO тут напишите цикл с вызовом FizzBuzz
 	// fmt.Println(fizzbuzz.FizzBuzz(10))
+	for i := 1; i < 101; i++ {
+		fmt.Println(fizzbuzz.FizzBuzz(i))
+	}
 }
