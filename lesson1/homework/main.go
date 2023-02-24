d0fb75f40d05efc527aa977e477b1a5445711088
Evindori <evindori@gmail.com>
2023-02-24 04:27:13 +0300

    Выполненое задание 1.0

--- lesson1/homework/main.go
+++ lesson1/homework/main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	// TODO тут напишите цикл с вызовом FizzBuzz
+	for i := 1; i < 101; i++ {
+		fmt.Println(fizzbuzz.FizzBuzz(i))
+	}
 	// fmt.Println(fizzbuzz.FizzBuzz(10))
+	// fmt.Println("Fizz")
 }
