24e084280731f9ac27f616af63ae4c4cd047f67b
ormequ <ilya.belokobylsky@gmail.com>
2023-02-24 14:54:31 +0300

    Homework #1: Done

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,16 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	switch {
+	case i%15 == 0:
+		return "FizzBuzz"
+	case i%3 == 0:
+		return "Fizz"
+	case i%5 == 0:
+		return "Buzz"
+	default:
+		return strconv.Itoa(i)
+	}
 }
