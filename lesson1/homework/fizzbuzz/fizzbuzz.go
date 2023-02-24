cfe6a50de3dbcb129a747d9cc10467aea4891dce
Alexandr Bondarenko <bon527870@mail.ru>
2023-02-24 23:11:56 +0300

    add [FizzBuzz] implementation

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	result := ""
+	if i%3 == 0 {
+		result += "Fizz"
+	}
+	if i%5 == 0 {
+		result += "Buzz"
+	}
+	if result == "" {
+		result = strconv.Itoa(i)
+	}
+	return result
 }
