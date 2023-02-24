963d5a8a8932e508acfb86496ee5eccec85fa4e7
GrusnyDance <grusny.dance@gmail.com>
2023-02-24 10:56:22 +0100

    done

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,18 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var ret string
+	if (i%3 != 0) && (i%5 != 0) {
+		ret = strconv.Itoa(i)
+	} else {
+		if i%3 == 0 {
+			ret = "Fizz"
+		}
+		if i%5 == 0 {
+			ret += "Buzz"
+		}
+	}
+	return ret
 }
