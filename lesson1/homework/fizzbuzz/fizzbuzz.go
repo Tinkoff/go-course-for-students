95ee307eebf440987a1c2a0930cdbe6d95df5390
rusteRR <alexvingra@mail.ru>
2023-02-25 17:13:31 +0300

    new: add HW-1 solution

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,18 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	if i%3 != 0 && i%5 != 0 {
+		return strconv.Itoa(i)
+	}
+
+	result := ""
+	if i%3 == 0 {
+		result += "Fizz"
+	}
+	if i%5 == 0 {
+		result += "Buzz"
+	}
+	return result
 }
--- lesson1/homework/main.go
+++ lesson1/homework/main.go
@@ -1,11 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"lecture01_homework/fizzbuzz"
-)
+import "lecture01_homework/fizzbuzz"
 
 func main() {
-	// TODO тут напишите цикл с вызовом FizzBuzz
-	// fmt.Println(fizzbuzz.FizzBuzz(10))
+	const (
+		iterateFrom int = 1
+		iterateTo   int = 100
+	)
+
+	for i := iterateFrom; i <= iterateTo; i++ {
+		println(fizzbuzz.FizzBuzz(i))
+	}
 }
