030f831c5dfe09857577fa04095ae1cd1c9235cb
Evindori <evindori@gmail.com>
2023-02-25 15:08:20 +0300

    Выполненное задание 1.2: Убрала лишний else, меньшила вложенность

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -6,14 +6,13 @@ func FizzBuzz(i int) string {
 	// TODO
 	ans := ""
 	if i%5 != 0 && i%3 != 0 {
-		ans = strconv.Itoa(i)
-	} else {
-		if i%3 == 0 {
-			ans += "Fizz"
-		}
-		if i%5 == 0 {
-			ans += "Buzz"
-		}
+		return strconv.Itoa(i)
+	}
+	if i%3 == 0 {
+		ans += "Fizz"
+	}
+	if i%5 == 0 {
+		ans += "Buzz"
 	}
 	return ans
 }
--- lesson1/homework/main.go
+++ lesson1/homework/main.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-	// TODO тут напишите цикл с вызовом FizzBuzz
 	for i := 1; i < 101; i++ {
 		fmt.Println(fizzbuzz.FizzBuzz(i))
 	}
-	// fmt.Println(fizzbuzz.FizzBuzz(10))
-	// fmt.Println("Fizz")
 }
