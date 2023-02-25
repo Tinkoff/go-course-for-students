7c31f33e8c7dc0d86e65f723e6f5c58af538788c
Юлия Koроленко <KorolenkoYulia>
2023-02-26 00:28:09 +0300

    first try

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,27 @@
 package fizzbuzz
 
+import "fmt"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+
+	var result string = ""
+
+	if i%3 == 0 {
+		result += "Fizz"
+	}
+	if i%5 == 0 {
+		result += "Buzz"
+	}
+
+	if result != "" {
+		return result
+	} else {
+		return string(i)
+	}
+}
+
+func main() {
+	for value := 0; value <= 100; value++ {
+		fmt.Println(FizzBuzz(value))
+	}
 }
