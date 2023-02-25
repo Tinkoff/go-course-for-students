1e0fef462f6f19aa2b47c4847a9f3180d2eea88e
RML7 <mraspisenko@bk.ru>
2023-02-25 22:03:36 +0300

    hw1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
+import (
+	"strconv"
+)
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	if i%3 == 0 && i%5 == 0 {
+		return "FizzBuzz"
+	} else if i%3 == 0 {
+		return "Fizz"
+	} else if i%5 == 0 {
+		return "Buzz"
+	}
+
+	return strconv.Itoa(i)
 }
