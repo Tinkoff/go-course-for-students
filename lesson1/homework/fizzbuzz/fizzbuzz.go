c175e3188d5e893be2ad1779d73d3a56ecdd1af5
plotnikovada <dmplotnikova@yandex.ru>
2023-02-26 22:50:09 +0300

    new file added

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,25 @@
 package fizzbuzz
 
+import (
+    "strconv"
+    "fmt"
+)
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	if i % 15 == 0 {
+        return "FizzBuzz"
+    }
+    if i % 5 == 0 {
+        return "Buzz"
+    }
+    if i % 3 == 0 {
+        return "Fizz"
+    }
+	return strconv.Itoa(i)
+}
+
+func main() {
+    for i := 1; i <= 100; i++ {
+        fmt.Printf("%v ", FizzBuzz(i));
+    }
 }
