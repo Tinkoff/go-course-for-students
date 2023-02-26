d49ef88a5ce897918cf2cf2dcdc1569b049a4de2
togrammer <toprogrammer@yandex.ru>
2023-02-26 15:39:48 +0300

    added HW1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	ans := ""
+	if i%3 == 0 {
+		ans += "Fizz"
+	}
+	if i%5 == 0 {
+		ans += "Buzz"
+	}
+	if i%3 != 0 && i%5 != 0 {
+		ans += strconv.Itoa(i)
+	}
+	return ans
 }
