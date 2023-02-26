d67ea1a85e1c243cf17a7b97421ef1525c1e42be
Timur_Q <ilyasovtimur2001@gmail.com>
2023-02-26 13:36:14 +0300

    Add solved first HomeWork :) bzzz

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
+import "fmt"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	result := fmt.Sprintf("%d", i)
+	if i%3 == 0 {
+		result = "Fizz"
+	}
+	if i%5 == 0 {
+		result = "Buzz"
+	}
+	if i%15 == 0 {
+		result = "FizzBuzz"
+	}
+	return result
 }
