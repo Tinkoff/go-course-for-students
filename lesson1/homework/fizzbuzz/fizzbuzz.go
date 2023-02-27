e142d1982d1949b263ebd3b42a56e298aa3165b1
InfinityMeta <maksim.golovin.2001@mail.ru>
2023-02-27 16:03:08 +0300

    hw-1, 2 version

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -4,15 +4,14 @@ import "strconv"
 
 func FizzBuzz(i int) string {
 	// TODO
-	if i%3 == 0 {
-		if i%5 == 0 {
-			return "FizzBuzz"
-		} else {
-			return "Fizz"
-		}
-	} else if i%5 == 0 {
-		return "Buzz"
-	} else {
-		return strconv.Itoa(i)
+	if i%15 == 0 {
+		return "FizzBuzz"
 	}
+	if i%5 == 0 {
+		return "Buzz"
+	}
+	if i%3 == 0 {
+		return "Fizz"
+	}
+	return strconv.Itoa(i)
 }
