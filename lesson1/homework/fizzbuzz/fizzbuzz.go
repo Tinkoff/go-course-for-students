d5bbece45305b63c9103e6ac77e7181d0d270bfd
Artyom Podogrny <azaztllo2015@yandex.ru>
2023-02-27 15:55:30 +0300

    Fix

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,11 +1,15 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
 	if i%15 == 0 {
 		return "FizzBuzz"
 	} else if i%3 == 0 {
 		return "Fizz"
-	} else {
+	} else if i%5 == 0 {
 		return "Buzz"
+	} else {
+		return strconv.Itoa(i)
 	}
 }
--- lesson1/homework/main.go
+++ lesson1/homework/main.go
@@ -7,10 +7,6 @@ import (
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		if i%3 == 0 || i%5 == 0 {
-			fmt.Println(fizzbuzz.FizzBuzz(i))
-		} else {
-			fmt.Println(i)
-		}
+		fmt.Println(fizzbuzz.FizzBuzz(i))
 	}
 }
