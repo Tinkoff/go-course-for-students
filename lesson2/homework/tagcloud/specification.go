c56ef364def8a6a498d40522631705af41c273bb
Danila Bohanov <danilabohanov@gmail.com>
2023-03-04 18:13:20 +0300

    task has been done

--- lesson2/homework/tagcloud/specification.go
+++ lesson2/homework/tagcloud/specification.go
@@ -1,7 +1,10 @@
 package tagcloud
 
+import "sort"
+
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
+	tagTable map[string]int
 	// TODO: add fields if necessary
 }
 
@@ -15,14 +18,17 @@ type TagStat struct {
 // TODO: You decide whether this function should return a pointer or a value
 func New() TagCloud {
 	// TODO: Implement this
-	return TagCloud{}
+	d := new(TagCloud)
+	d.tagTable = make(map[string]int)
+	return *d
 }
 
 // AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
 // thread-safety is not needed
 // TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) AddTag(tag string) {
+func (d *TagCloud) AddTag(tag string) {
 	// TODO: Implement this
+	d.tagTable[tag] += 1
 }
 
 // TopN should return top N most frequent tags ordered in descending order by occurrence count
@@ -31,7 +37,18 @@ func (TagCloud) AddTag(tag string) {
 // thread-safety is not needed
 // there are no restrictions on time complexity
 // TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) TopN(n int) []TagStat {
+func (d *TagCloud) TopN(n int) []TagStat {
 	// TODO: Implement this
-	return nil
+	topList := make([]TagStat, 0, len((*d).tagTable))
+
+	for key, val := range (*d).tagTable {
+		topList = append(topList, TagStat{key, val})
+	}
+	sort.Slice(topList, func(i, j int) bool {
+		return topList[i].OccurrenceCount > topList[j].OccurrenceCount
+	})
+	for len(topList) > n {
+		topList = topList[:len(topList)-1]
+	}
+	return topList
 }
